38cbfd811563a03fea8f3860660cb0b7bba28ee2
Mikhail Petrenko <greenfall1@gmail.com>
2023-11-29 20:41:24 +0300

    29.11.23 ifelse

---
+++ lesson_2_loop_logic_operators_functions/IfElse/ifelse_4.js
@@ -0,0 +1,10 @@
+// https://learn.javascript.ru/task/rewrite-if-question
+// Перепишите конструкцию if с использованием условного оператора '?':
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+let result = a + b < 4 ? 'Мало' : 'Много'
