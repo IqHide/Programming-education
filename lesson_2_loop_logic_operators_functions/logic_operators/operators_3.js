b5d7df96ca9c24b950bb5ef1ce6495586d1b4f8f
Mikhail Petrenko <greenfall1@gmail.com>
2023-11-29 19:02:26 +0300

    29.11.23 logic operators and comparsion

---
+++ lesson_2_loop_logic_operators_functions/logic_operators/operators_3.js
@@ -0,0 +1,16 @@
+// https://learn.javascript.ru/task/primitive-conversions-questions
+// Какой результат будет у выражений ниже?
+'' + 1 + 0 //10 oshibsya
+'' - 1 + 0 //-1
+true + false // 1
+6 / '3' // 2
+'2' * '3' //6
+4 + 5 + 'px' //9px oshibsya
+'$' + 4 + 5 //$45 oshibsya
+'4' - 2 //2
+'4px' - 2 // NaN oshibsya
+'  -9  ' + 5 //-9  5 oshibsya
+'  -9  ' - 5 // -14
+null + 1 // 1
+undefined + 1 // NaN
+' \t \n' - 2 // -2 oshibsya
