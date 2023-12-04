6bac30acd74c4c42358e5847c65bfce5b2e7a091
Viktor Palchynskyi <viktor.palchynskyi@fivewalls.com.ua>
2023-12-04 13:42:53 +0300

    04.12.23 lesson 2: checkout

---
+++ lesson_2_loop_logic_operators_functions/index.js
@@ -0,0 +1,44 @@
+// const arr = [1, 2, 4];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`arr: ${arr[i]}`);    
+// }
+
+const a = 10;
+const b = 12;
+
+// a > b ? getSome() : getAnother();
+
+switch (10) {
+    case 1:
+        console.log(1);
+        break;
+    case 2: 
+    case 3:
+        console.log(5);
+    default:
+        break;
+}
+
+const tmp = 'misha';
+
+console.log('misha say: ', 10 || 22 || tmp);
+
+const some = { name: '', surname: 'Petrenko' };
+
+const name = some.name || some.surname;
+
+if (!(10 > a)) {
+
+}
+
+const abc =  function some(a, b = 20) {
+    return a + b;
+};
+
+let age = prompt('Help');
+checkAge(+age);
+function checkAge(age) {
+    console.log(typeof age);
+	age = age >= 18 || confirm('Родители разрешили?')
+}
