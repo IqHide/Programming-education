f6c5416db6783695864fc4f46e17766ff4e6ac47
Kartbllansh <movolovo93@gmail.com>
2024-04-03 12:27:39 +0300

    03.04.2024 Types: task 1

---
+++ Homework/Artemiy/Types data/task1.js
@@ -0,0 +1,16 @@
+let name = 'Ilya' //Инициализация и присваивание значение переменной
+
+alert(`hello ${1}`) // // Вывод: hello 1
+
+alert(`hello ${'name'}`) // Вывод: hello name
+
+alert(`hello ${name}`) // Вывод: hello Ilya
+
+//Код для проверки своих ответов
+
+console.log(`hello ${1}`)
+console.log(`hello ${'name'}`)
+console.log(`hello ${name}`)
+
+// Скрипт запускаю командой - node <имя файла>
+//Стоит отметить, что, судя по всему, за счет Node.js нельзя запускать функцию alert. Ее поддерживают только браузеры
