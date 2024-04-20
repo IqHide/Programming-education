b16657d8a5e98ad944974b6dbca004fe2d20c5e9
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2024-04-20 22:20:09 +0300

    20.04.24 function-basics: task 3-4

--- Homework/Nikita/function-basics/tasks.js
+++ Homework/Nikita/function-basics/tasks.js
@@ -9,3 +9,27 @@ function checkAge(age) {
     return (age > 18) ? true : confirm("Родители разрешили");
 }
 
+// task 3
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
+// task 4 
+function pow(x, n) {
+    if (n > 0) {
+        return x * pow(x, n-1);
+    }
+    else if (n == 0) {
+        return 1
+    }
+    return -1;
+}
+
+function pow(x, n) {
+    result = 1;
+    for (int i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
