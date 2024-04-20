fdc6122e96ef3e3f09830a2da3f61167eee6e8fb
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2024-04-20 23:07:54 +0300

    lesson task

--- Homework/Nikita/lesson_3/task.js
+++ Homework/Nikita/lesson_3/task.js
@@ -1,32 +1,32 @@
-// ошибки + декомпозиция
-function create_object() {
+function create_object(s, num) {
+    return {
+        [s]: num,
+        error: false,
+    }
 }
+
+function create_object_error() {
+    return {
+        error: true,
+    }
+}
+
 function foo(num1, num2, s) {
-    if (typeof(num1) != Number || typeof(num2) != Number) {
-        return {
-        }
+    if (typeof(num1) != "number" || typeof(num2) != "number") {
+        return create_object_error();
     }
     let a1 = num1;
     let a2 = num2;
     if (num1 + num2 > 3) {
-        return {
-            [s]: num1 * num2,
-        }
+        return create_object(s, num1*num2);
     } else if (num1 + num2 < 3) {
         while (a1 + a2 != 3) {
             a1++;
         }
-
-        return {
-            Nikita: 19,
-        }
-
+        return create_object("Nikita", 19);
     } else {
-        return {
-            Nikita: 19,
-        }
-
+        return create_object("Nikita", 19);
     }
 };
 
-console.log(foo(1, 1, 'uuaaa'));
+console.log(foo(5, 1, 'uuaaa'));
