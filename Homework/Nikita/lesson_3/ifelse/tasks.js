82427d112b9385935c2c8ec4be043882a78b1dbb
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2024-04-20 21:55:56 +0300

    20.04.24 ifelse: task 1-3

---
+++ Homework/Nikita/lesson_3/ifelse/tasks.js
@@ -0,0 +1,29 @@
+//task 1
+if ("0") {
+  alert( 'Привет' );
+}
+// выведется
+
+// task 2
+let js_off_name = "ECMAScript";
+
+if (js_off_name === "ECMAScript") {
+    conole.log("Верно!");
+}
+else {
+    conole.log("Не знаете? ECMAScript!");
+}
+
+// task 3
+let val = 10;
+
+if (val > 0) {
+    console.log(1);
+}
+else if (val < 0) {
+    console.log(-1);
+}
+else {
+    console.log(0);
+}
+
