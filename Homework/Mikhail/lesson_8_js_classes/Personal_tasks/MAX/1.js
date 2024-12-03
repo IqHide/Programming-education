d4f6af2a4de58d052039274f0aa6d315b024ed5a
Mikhail Petrenko <greenfall1@gmail.com>
2024-12-03 19:26:32 +0300

    03.12.2024: lesson_8_classes Personal_tasks MAX

--- Homework/Mikhail/lesson_8_js_classes/Personal_tasks/MAX/1.js
+++ Homework/Mikhail/lesson_8_js_classes/Personal_tasks/MAX/1.js
@@ -9,3 +9,40 @@
 // Manager, который добавляет поле department (отдел) и переопределяет метод getDetails так, чтобы включить отдел в описание.
 // Developer, который добавляет поле programmingLanguage (основной язык программирования) и переопределяет метод getDetails, чтобы включить язык программирования в описание.
 // Задание: Создай экземпляры Manager и Developer, вызови для них метод getDetails, а также используй compareSalaries для сравнения их зарплат.
+class Employee {
+	constructor(name, salary) {
+		this._name = name;
+		this._salary = salary;
+	}
+	getDetails() {
+		return `Employee: ${_name}, Salary: ${_salary}`;
+	}
+	static compareSalaries(emp1, emp2) {
+		return emp1._salary > emp2._salary
+			? console.log(`${emp1._name}`)
+			: console.log(`${emp2._name}`);
+	}
+}
+class Manager extends Employee {
+	constructor(name, salary, dep) {
+		super(name, salary);
+		this.department = dep;
+	}
+	getDetails() {
+		return `Employee: ${this._name}, Salary: ${this._salary}, Department: ${this.department}`;
+	}
+}
+class Developer extends Employee {
+	constructor(name, salary, lang) {
+		super(name, salary);
+		this.programmingLanguage = lang;
+	}
+	getDetails() {
+		return `Employee: ${this._name}, Salary: ${this._salary}, ProgrammingLanguage: ${this.programmingLanguage}`;
+	}
+}
+let manager = new Manager('John', 35000, 'marketing');
+let developer = new Developer('Adolf', 999000, 'JS');
+console.log(manager.getDetails());
+console.log(developer.getDetails());
+Employee.compareSalaries(manager, developer);
